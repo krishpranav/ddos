913d2d789b53906576a54635fa884bb8c5d7d33e
krishpranav <krisna.pranav@gmail.com>
2021-04-19 12:38:13 +0530

    update ddos script

--- ddos_test.go
+++ ddos_test.go
@@ -22,3 +22,27 @@ func TestNewDDoS(t *testing.T) {
 		t.Error("Cannot create a new ddos structure")
 	}
 }
+
+func TestDDoS(t *testing.T) {
+	port, err := freeport.Get()
+	if err != nil {
+		t.Errorf("Cannot found free tcp port. Error = %v", err)
+	}
+	createServer(port, t)
+
+	url := "http://127.0.0.1:" + strconv.Itoa(port)
+	d, err := ddos.New(url, 1000)
+	if err != nil {
+		t.Error("Cannot create a new ddos structure")
+	}
+	d.Run()
+	time.Sleep(time.Second)
+	d.Stop()
+	success, amount := d.Result()
+	if success == 0 || amount == 0 {
+		t.Errorf("Negative result of DDoS attack.\n"+
+			"Success requests = %v.\n"+
+			"Amount requests = %v", success, amount)
+	}
+	t.Logf("Statistic: %d %d", success, amount)
+}
